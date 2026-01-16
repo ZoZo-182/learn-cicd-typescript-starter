1dc4bfbf0dbb3d7c09b4f7c4c1a79103bb3cce0a
ZoZo-182 <robinsoz@clarkson.edu>
2026-01-15 23:55:09 -0500

    force fail

---
+++ src/tests/getAPIKey.test.ts
@@ -0,0 +1,8 @@
+import { expect, test } from "vitest";
+import { getAPIKey } from "../api/auth.ts";
+
+const header = {};
+
+test('returns null because empty header', () => {
+    expect(getAPIKey(header)).toBe(nulll);
+})
